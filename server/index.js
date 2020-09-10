5f9fdc49a8a464aad86899e66a488e33da7d14cf
bVelliquette <BenjaminVelliquette@gmail.com>
2020-09-10 09:59:42 -0400

    prepare for heroku

--- server/index.js
+++ server/index.js
@@ -19,10 +19,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
 
 app.use("/api/schedule", scheduleRouter);
 app.use("/api/game", gameRouter);
+
+//Handle Production
+if(process.env.NODE_ENV === 'production')
+{
+  //Static folder
+  app.use(express.static(__dirname + '/public'));
+
+  //Handle SPA
+  app.get(/.*/, (req,res)=> res.sendFile(__dirname + 'public/index.html'));
+}
+
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
